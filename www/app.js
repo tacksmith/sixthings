f16f3f97ebf6fd71755406f542ff948a3520859f
nero985 <nero985@gmail.com>
2026-08-26 14:28:18 +0800

    Release 20260824q

--- www/app.js
+++ www/app.js
@@ -7,7 +7,7 @@
 
 const KEY = "sixthings:v1";
 // 应用版本号（与 index.html 的 ?v= 保持同步）
-const APP_VERSION = "20260824o";
+const APP_VERSION = "20260824q";
 
 /* ---------------- 状态 ---------------- */
 let S = load();
